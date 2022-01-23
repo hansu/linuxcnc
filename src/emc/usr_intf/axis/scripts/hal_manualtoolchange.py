c03495935eebab13656811a600265a9edb4337ec
Hans Unzner <hansunzner@gmail.com>
2022-01-23 18:47:07 +0100

    added argumentparsing

--- src/emc/usr_intf/axis/scripts/hal_manualtoolchange.py
+++ src/emc/usr_intf/axis/scripts/hal_manualtoolchange.py
@@ -4,17 +4,44 @@ import gettext
 import linuxcnc, hal
 import nf, rs274.options
 import tkinter
-import gi
-gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
-from gi.repository import GLib
+import argparse
 
 BASE = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), ".."))
 gettext.install("linuxcnc", localedir=os.path.join(BASE, "share", "locale"))
 
+parser = argparse.ArgumentParser(description='HAL Manual toolchanger')
+parser.add_argument('--gui', type=str, metavar='DISPLAY',  
+                    help='User interface corresponding to [DISPLAY] value in inifile (axis, gmoccapy, gscreen ...)',
+                    default=None)
+parser.add_argument('--force_toolkit', type=str, metavar='TOOLKIT',   
+                    help='Force toolkit (Tk/Gtk), ignores gui-setting',
+                    default=None)           
+opts = parser.parse_args(sys.argv[1:])                                          
+                                                
+inifile = linuxcnc.ini(os.environ.get('INI_FILE_NAME', '/dev/null'))
+display = inifile.find("DISPLAY","DISPLAY")
+gtk_strings = ["gmoccapy", "gscreen", "touchy", "qtvcp", "qtvcp woodpecker"]
+
+if opts.force_toolkit in ["Gtk", "gtk", "GTK"]:
+    use_gtk = True
+elif opts.force_toolkit in ["Tk", "tk", "TK"]:    
+    use_gtk = False
+else:
+    if opts.gui in gtk_strings:
+        use_gtk = True
+    elif display in gtk_strings:
+        use_gtk = True
+    else:
+        use_gtk = False
+
+if use_gtk:
+    import gi
+    gi.require_version("Gtk", "3.0")
+    from gi.repository import Gtk
+    from gi.repository import GLib
+
 _after = None
 def poll_hal_in_background():
-    # print("poll hal in background")
     global _after
     _after = None
     if not h.change:
@@ -40,7 +67,6 @@ def stop_polling_hal_in_background():
     _after = None
 
 def periodic():
-    # print("periodic")
     global dialog
     if not h.change:
         dialog.response(Gtk.ResponseType.ACCEPT)
@@ -63,7 +89,7 @@ def do_change(n):
 
     global dialog
     try:
-        if display in ["gmoccapy", "gscreen", "touchy"]:  #- qtvcp, qtvcp woodpecker? 
+        if use_gtk:
             win = Gtk.Window()
             dialog = Gtk.Dialog(_("Tool change"), win, None)
             dialog.connect("delete-event", lambda w, d: w.run())
@@ -71,13 +97,13 @@ def do_change(n):
             label = Gtk.Label.new(message)
             label.set_line_wrap(True)
             button = Gtk.Button.new_with_mnemonic(_("_Continue"))
-            button.set_size_request(-1, 60)
+            button.set_size_request(-1, 56)
             button.connect("clicked",lambda w:dialog.response(Gtk.ResponseType.OK))
             box = Gtk.HButtonBox()
             box.add(button)
             dialog.vbox.pack_start(label, True, True, 0)
             dialog.vbox.pack_end(box, True, True, 0)
-            # dialog.set_border_width(0)
+            dialog.set_border_width(0)
             dialog.show_all()
             timer_id = GLib.timeout_add(100, periodic)
             r = dialog.run()
@@ -91,8 +117,8 @@ def do_change(n):
             win.show_all()
 
         else:
-            # with nf_dialog following error: _tkinter.TclError: bad screen distance ".25"
-            r = app.tk.call("tk_dialog", ".tool_change",
+            # with nf_dialog and Gtk import following error: _tkinter.TclError: bad screen distance ".25"
+            r = app.tk.call("nf_dialog", ".tool_change",
                 _("Tool change"), message, "info", 0, _("Continue"))
     finally:
         stop_polling_hal_in_background()
@@ -112,15 +138,12 @@ h.newpin("change_button", hal.HAL_BIT, hal.HAL_IN)
 h.newpin("changed", hal.HAL_BIT, hal.HAL_OUT)
 h.ready()
 
-inifile = linuxcnc.ini(os.environ.get('INI_FILE_NAME', '/dev/null'))
-display = inifile.find("DISPLAY","DISPLAY")
-print("Manualtoolchanger is loaded from", display)
 
 app = tkinter.Tk(className="AxisToolChanger")
 app.wm_geometry("-60-60")
 app.wm_title(_("AXIS Manual Toolchanger"))
 rs274.options.install(app)
-# nf.start(app); nf.makecommand(app, "_", _)
+nf.start(app); nf.makecommand(app, "_", _)
 app.wm_protocol("WM_DELETE_WINDOW", app.wm_withdraw)
 lab = tkinter.Message(app, aspect=500, text = _("\
 This window is part of the AXIS manual toolchanger.  It is safe to close \
